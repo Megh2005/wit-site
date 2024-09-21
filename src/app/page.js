716940fe7c793247a0b33fb422ab894d9b92256a
Megh Deb <megh.deb.civil27@heritageit.edu.in>
2024-09-21 20:41:59 +0530

    committed

--- src/app/page.js
+++ src/app/page.js
@@ -1,62 +1,213 @@
 "use client";
+
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
-const HomePage = () => {
+const LoginForm = () => {
+  const styles = {
+    general: {
+      margin: 0,
+      padding: 0,
+      boxSizing: "border-box",
+      fontFamily: "'Poppins', sans-serif",
+    },
+    body: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      minHeight: "100vh",
+      background:
+        "url(https://res.cloudinary.com/dmbxx03vp/image/upload/v1726831166/bg_c_oeldnc.png) no-repeat center/cover",
+    },
+    wrapper: {
+      width: "90%",
+      maxWidth: "420px",
+      background: "transparent",
+      border: "2px solid rgba(0, 0, 0, 0.2)",
+      backdropFilter: "blur(9px)",
+      color: "#000",
+      borderRadius: "12px",
+      padding: "20px",
+    },
+    h1: {
+      fontSize: "28px",
+      textAlign: "center",
+    },
+    inputBox: {
+      position: "relative",
+      width: "100%",
+      height: "50px",
+      margin: "20px 0",
+    },
+    input: {
+      width: "100%",
+      height: "100%",
+      background: "transparent",
+      outline: "none",
+      border: "2px solid rgba(0, 0, 0, 0.2)",
+      borderRadius: "40px",
+      fontSize: "14px",
+      color: "#000",
+      padding: "20px 45px 20px 20px",
+    },
+    inputPlaceholder: {
+      color: "#000",
+    },
+    btn: {
+      width: "100%",
+      height: "45px",
+      background: "#edde0e",
+      border: "none",
+      outline: "none",
+      borderRadius: "40px",
+      boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+      cursor: "pointer",
+      fontSize: "16px",
+      color: "#d13f17",
+      fontWeight: 600,
+    },
+    registerLink: {
+      fontSize: "14px",
+      textAlign: "center",
+      margin: "20px 0 15px",
+    },
+    a: {
+      color: "#000",
+      textDecoration: "none",
+    },
+    aHover: {
+      textDecoration: "underline",
+    },
+    media768: {
+      wrapper: {
+        padding: "20px",
+      },
+      h1: {
+        fontSize: "24px",
+      },
+      inputBox: {
+        margin: "15px 0",
+      },
+      input: {
+        fontSize: "14px",
+      },
+      btn: {
+        fontSize: "15px",
+      },
+      registerLink: {
+        fontSize: "13px",
+      },
+    },
+    media480: {
+      wrapper: {
+        padding: "15px",
+      },
+      h1: {
+        fontSize: "20px",
+      },
+      inputBox: {
+        margin: "10px 0",
+      },
+      input: {
+        fontSize: "12px",
+        padding: "15px 40px 15px 15px",
+      },
+      btn: {
+        fontSize: "14px",
+      },
+      registerLink: {
+        fontSize: "12px",
+      },
+    },
+  };
+
   const router = useRouter();
-  const handleLoginPage = () => {
-    router.push("/login");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+
+  const submitLoginForm = async () => {
+    setSubmitting(true);
+
+    const result = await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+    });
+
+    setSubmitting(false);
+
+    if (result?.error) {
+      toast.error(result.error);
+    }
+
+    if (result?.ok) {
+      toast.success("Logged in Successfully");
+      console.log(`Logged In`);
+      router.replace("/home");
+    }
   };
-  const handleContactPage = () => {
-    router.push("/contact");
-  };
-  const handleRegisterPage = () => {
-    router.push("/register");
-  };
-  const handleHomePage = () => {
-    router.push("/home");
-  }
 
   return (
-    <div
-      className="min-h-screen flex flex-col items-center justify-center px-4"
-      style={{
-        backgroundImage: `url(https://res.cloudinary.com/dmbxx03vp/image/upload/v1726831165/bg_b_x4nfmk.png)`, // Set the background image
-        backgroundSize: "cover", // Ensure the image covers the entire screen
-        backgroundPosition: "center", // Center the image
-        backgroundRepeat: "no-repeat", // Prevent the image from repeating
-      }}
-    >
-      <div className="relative uppercase bg-white/30 backdrop-blur-lg p-8 rounded-lg shadow-lg text-center">
-        <h1 className="text-3xl md:text-4xl font-bold text-purple-700 mb-8">
-          Welcome To WIT Dreamin 2024 <br />
-          <span className="text-pink-600  font-extrabold text-6xl space-y-9">
-            কলকাতা
-          </span>
-        </h1>
-        {/* Add justify-center to center buttons */}
-        <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-4">
+    <div style={styles.body}>
+      <div style={styles.wrapper}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            submitLoginForm();
+          }}
+        >
+          <h1 style={styles.h1}>
+            <span style={{ color: "#000", fontWeight: "800" }}>
+              Welcome To
+              <br />
+            </span>
+            <span style={{ color: "#FF0000", fontWeight: "800" }}>কলকাতা</span>
+            <hr width="true" style={{ color: "#000" }} />
+          </h1>
+          <div style={styles.inputBox}>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="Email Address"
+              required
+              style={styles.input}
+            />
+          </div>
+          <div style={styles.inputBox}>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              minLength={8}
+              maxLength={16}
+              placeholder="Password"
+              required
+              style={styles.input}
+            />
+          </div>
           <button
-            onClick={handleLoginPage}
-            className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-3 px-6 rounded-full focus:outline-none focus:ring-4 focus:ring-purple-500 transition duration-300 w-full md:w-auto"
+            type="submit"
+            className="hover:bg-gray-600 hover:text-white flex justify-center items-center"
+            style={{
+              ...styles.btn,
+              color: "#000",
+              fontWeight: "800",
+            }}
           >
-            Get Started
+            {submitting ? (
+              <div className="loading loading-spinner loading-sm text-black"></div>
+            ) : (
+              "Sign In"
+            )}
           </button>
-          <button
-            onClick={handleContactPage}
-            className="bg-purple-700 hover:bg-purple-800 text-white font-bold py-3 px-6 rounded-full focus:outline-none focus:ring-4 focus:ring-pink-500 transition duration-300 w-full md:w-auto"
-          >
-            Need Support
-          </button>
-          {/* <button
-            onClick={handleRegisterPage}
-            className="bg-red-500 hover:bg-red-800 text-white font-bold py-3 px-6 rounded-full focus:outline-none focus:ring-4 focus:ring-pink-500 transition duration-300 w-full md:w-auto"
-          >
-            Register User
-          </button> */}
-        </div>
+        </form>
       </div>
     </div>
   );
 };
 
-export default HomePage;
+export default LoginForm;
