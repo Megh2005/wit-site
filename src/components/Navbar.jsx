fe602427580b4329ac5b50bab6f88d2f8be36a8d
Megh Deb <megh.deb.civil27@heritageit.edu.in>
2024-09-21 21:28:22 +0530

    modifying button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
       <div>
         <button
           onClick={() => signOut({ callbackUrl: "/login" })}
-          className="text-white font-semibold bg-red-500 hover:bg-red-700 px-4 py-2 rounded"
+          className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
         >
           Log Out
         </button>
