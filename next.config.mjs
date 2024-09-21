3c40d7f12bdf8b0d9ee7e4aac9792f646cc3e3d4
Megh Deb <megh.deb.civil27@heritageit.edu.in>
2024-09-21 16:56:09 +0530

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"], // Allow Cloudinary images
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dmbxx03vp/**", // Adjust the pathname if necessary
+      },
+    ],
   },
 };
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-import Navbar from "@/Components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
