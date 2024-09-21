631b91f9033f39b73c5ed8cc91477341724ca952
Megh Deb <megh.deb.civil27@heritageit.edu.in>
2024-09-21 16:31:06 +0530

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,6 @@
 import { signOut } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 const Navbar = () => {
   return (
@@ -39,7 +38,7 @@ const Navbar = () => {
         </Link>
         <button
           onClick={() => signOut({ callbackUrl: "/login" })}
-          className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          className="text-white font-semibold bg-red-500 hover:bg-red-700 px-4 py-2 rounded"
         >
           Log Out
         </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      colors: {},
     },
   },
   plugins: [],
