b5812d8792d6777206080c3dc120e2ddaca8f8e1
Megh Deb <megh.deb.civil27@heritageit.edu.in>
2024-09-25 14:40:07 +0530

    Color correction

--- src/app/(main)/agenda/page.jsx
+++ src/app/(main)/agenda/page.jsx
@@ -24,7 +24,7 @@ const AgendaPage = () => {
       time: "01:00 PM",
       title: "Lunch Break",
       speaker: "N/A",
-      gradient: "bg-gradient-to-r from-purple-500 to-yellow-400",
+      gradient: "bg-gradient-to-r from-purple-500 to-blue-800",
     },
     {
       time: "02:00 PM",
@@ -41,7 +41,7 @@ const AgendaPage = () => {
   ];
 
   return (
-    <div className="min-h-[60vh] bg-gray-100 flex items-center justify-center p-4">
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
       <div className="max-w-4xl w-full">
         <h1 className="text-3xl font-extrabold text-center text-gray-800 py-4">
           Event Agenda
@@ -52,10 +52,10 @@ const AgendaPage = () => {
               key={index}
               className={`flex flex-col md:flex-row md:justify-between md:items-center py-3 px-5 rounded-lg shadow-md text-white ${item.gradient} hover:opacity-95 transition-opacity duration-200`}
             >
-              <div className="md:w-1/3 mb-2 md:mb-0 text-base font-bold text-center md:text-left">
+              <div className="md:w-1/3 text-base font-bold text-center md:text-left">
                 {item.time}
               </div>
-              <div className="md:w-1/3 text-lg font-extrabold text-center justify-center md:text-right">
+              <div className="md:w-1/3 text-lg font-extrabold text-center">
                 {item.title}
               </div>
               <div className="md:w-1/3 text-sm font-medium text-center md:text-right">
