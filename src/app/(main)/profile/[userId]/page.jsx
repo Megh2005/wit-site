eb6961cc68af624c9d18b283d436a354758fa12d
Megh Deb <megh.deb.civil27@heritageit.edu.in>
2024-09-25 07:08:27 +0530

    QR code

--- src/app/(main)/profile/[userId]/page.jsx
+++ src/app/(main)/profile/[userId]/page.jsx
@@ -40,7 +40,7 @@ const ProfilePage = () => {
               size={300}
               level="L"
               className="rounded-lg w-full object-cover"
-              value={`http://localhost:3000/profile/${session?.user.id}`}
+              value={`https://wit-dreamin-kolkata-2023.vercel.app/profile/${session?.user.id}`}
             />
           </div>
         </div>
