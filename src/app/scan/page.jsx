1ae5e738e4a3d92bc79425ab59c3b490e42db68f
Subham Mani <manisubham09@gmail.com>
2024-09-25 12:54:21 +0530

    update

--- src/app/scan/page.jsx
+++ src/app/scan/page.jsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { Scanner } from "@yudiel/react-qr-scanner";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import React, { useState } from "react";
+import { QrReader } from "react-qr-reader";
 
 const ScanPage = () => {
   const [scanValue, setScanValue] = useState("");
@@ -16,10 +16,21 @@ const ScanPage = () => {
         </Link>
       </div>
       <div className="my-6 w-full max-w-2xl mx-auto">
-        <Scanner onScan={(result) => setScanValue(result)} />
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setScanValue(result?.text);
+            }
+
+            if (!!error) {
+              console.info(error);
+            }
+          }}
+          style={{ width: "100%" }}
+        />
       </div>
       <div className="mt-6">
-        <p>{scanValue}</p>
+        <p>User id: {scanValue}</p>
       </div>
     </div>
   );
