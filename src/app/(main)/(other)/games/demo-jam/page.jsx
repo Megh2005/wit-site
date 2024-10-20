9b1e64767ec89214f9ecb92805449dcac8fc069b
Megh2005 <megh.deb.civil27@heritageit.edu.in>
2024-10-21 00:28:31 +0530

    Refactor demo jam waiting page to update start time

--- src/app/(main)/(other)/games/demo-jam/page.jsx
+++ src/app/(main)/(other)/games/demo-jam/page.jsx
@@ -5,7 +5,7 @@ const GameTwo = () => {
     <div className="w-full h-screen bg-black flex flex-col items-center justify-center">
       <h1 className="text-3xl capitalize sm:text-4xl px-12 font-bold mb-6 sm:mb-8 text-center bg-gradient-to-r from-green-400 to-green-600 text-transparent bg-clip-text">
         Demo Jam 
-        <span>Will Start On 17:00</span>
+        <span>Will Start On 17:15</span>
         <hr/>
         You have to use your cellular data to participate in demo jam
       </h1>
