274bb9c7d41d61abd5e9e9d57d826badbef66b41
Subham Mani <manisubham09@gmail.com>
2024-10-23 17:15:09 +0530

    add support for one login

--- src/app/(main)/home/page.jsx
+++ src/app/(main)/home/page.jsx
@@ -5,6 +5,9 @@ import { signOut, useSession } from "next-auth/react";
 import { FcRating } from "react-icons/fc";
 import Link from "next/link";
 import { LoaderCircle } from "lucide-react";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "@/services/firebaseinit";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
   const { data: session } = useSession();
@@ -113,6 +116,26 @@ const HomePage = () => {
     queryFn: getCoinBalance,
   });
 
+  const handleSignOut = async () => {
+    if (!session?.user) return;
+    try {
+      const userRef = doc(db, "users", session.user.id);
+      const user = await getDoc(userRef);
+
+      if (user.data().activeSession) {
+        await updateDoc(userRef, {
+          activeSession: false,
+        });
+
+        await signOut({ callbackUrl: "/" });
+      } else {
+        toast.error("Error signing out");
+      }
+    } catch (error) {
+      toast.error("Error signing out");
+    }
+  };
+
   if (!session?.user) {
     return (
       <div>
@@ -134,30 +157,28 @@ const HomePage = () => {
 
       <div className="px-2 justify-between gap-2 flex items-center text-center">
         <div className="bg-gradient-to-r from-gray-900 to-emerald-600 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-teal-200 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 shadow-md transition duration-300 ease-in-out">
-          <p className="flex items-center text-lg sm:text-xl gap-1 justify-center text-white">
-            <span>Balance :</span>
+          <div className="flex items-center text-base line-clamp-1 sm:text-xl gap-1 justify-center text-white">
             {isLoading ? (
               <LoaderCircle className="w-5 h-5 animate-spin text-white" />
             ) : (
               isSuccess && (
-                <>
-                  <span>{coins.data}</span>
-                  <FcRating className="text-2xl" />
-                </>
+                <div className="flex items-center gap-2">
+                  <span>Balance: {coins.data}</span>
+                  <FcRating className="w-6 h-6" />
+                </div>
               )
             )}
             {isError && (
-              <p>
+              <div>
                 <span>Error</span>
-                <FcRating className="text-2xl" />
-              </p>
+              </div>
             )}
-          </p>
+          </div>
         </div>
         <div className="flex justify-end p-4">
           <button
-            onClick={() => signOut({ callbackUrl: "/" })}
-            className="bg-gradient-to-r from-gray-900 to-emerald-600 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-teal-200 dark:focus:ring-teal-800 font-medium rounded-lg px-5 py-2.5 text-center mb-2 shadow-md transition duration-300 ease-in-out text-white text-lg sm:text-xl"
+            onClick={handleSignOut}
+            className="bg-gradient-to-r from-gray-900 to-emerald-600 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-teal-200 dark:focus:ring-teal-800 font-medium rounded-lg px-5 py-2.5 text-center mb-2 shadow-md transition duration-300 ease-in-out text-white text-base sm:text-xl"
           >
             Log Out
           </button>
