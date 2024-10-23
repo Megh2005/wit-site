00beef14d27df76c2d68fd23ba6633890b4048bf
Megh2005 <megh.deb.civil27@heritageit.edu.in>
2024-10-24 02:46:06 +0530

    Refactor page component: Update StrawPoll embed in Demo Jam page

--- src/app/(main)/(other)/games/demo-jam/page.jsx
+++ src/app/(main)/(other)/games/demo-jam/page.jsx
@@ -4,15 +4,15 @@ const GameTwo = () => {
   return (
     <div className="w-full h-screen bg-black flex flex-col items-center justify-center">
       <h1 className="text-3xl capitalize sm:text-4xl px-12 font-bold mb-6 sm:mb-8 text-center bg-gradient-to-r from-green-400 to-green-600 text-transparent bg-clip-text">
-        Demo Jam 
+        Demo Jam
         <span>Will Start On 17:15</span>
-        <hr/>
+        <hr />
         You have to use your cellular data to participate in demo jam
       </h1>
 
       {/* <div
         className="strawpoll-embed w-full h-[60vh] sm:h-[70vh] max-w-xs sm:max-w-lg flex flex-col"
-        id="strawpoll_7rnzVxjwlnO"
+        id="strawpoll_7MZ0kovwmgo"
         style={{
           width: "100%",
           margin: "0 auto",
@@ -22,8 +22,8 @@ const GameTwo = () => {
       >
         <iframe
           title="StrawPoll Embed"
-          id="strawpoll_iframe_7rnzVxjwlnO"
-          src="https://strawpoll.com/embed/7rnzVxjwlnO"
+          id="strawpoll_iframe_7MZ0kovwmgo"
+          src="https://strawpoll.com/embed/7MZ0kovwmgo"
           className="w-full h-full border-none"
           style={{
             position: "static",
@@ -37,7 +37,11 @@ const GameTwo = () => {
         >
           Loading...
         </iframe>
-        <script async src="https://cdn.strawpoll.com/dist/widgets.js"></script>
+        <script
+          async
+          src="https://cdn.strawpoll.com/dist/widgets.js"
+          charSet="utf-8"
+        ></script>
       </div> */}
     </div>
   );
