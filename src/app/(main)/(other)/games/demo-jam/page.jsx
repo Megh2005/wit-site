c899b074a5879e1fcb6a1ee6d3ed0f9102602bd9
Megh2005 <megh.deb.civil27@heritageit.edu.in>
2024-10-26 17:12:01 +0530

    Remove commented out code for the "Games" tile in the home page

--- src/app/(main)/(other)/games/demo-jam/page.jsx
+++ src/app/(main)/(other)/games/demo-jam/page.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const GameTwo = () => {
   return (
     <div className="w-full h-screen bg-black flex flex-col items-center justify-center">
-      <h1 className="text-3xl capitalize sm:text-4xl px-12 font-bold mb-6 sm:mb-8 text-center bg-gradient-to-r from-green-400 to-green-600 text-transparent bg-clip-text">
+      {/* <h1 className="text-3xl capitalize sm:text-4xl px-12 font-bold mb-6 sm:mb-8 text-center bg-gradient-to-r from-green-400 to-green-600 text-transparent bg-clip-text">
         Demo Jam
         <span>Will Start On 17:15</span>
         <hr />
         You have to use your cellular data to participate in demo jam
-      </h1>
+      </h1> */}
 
-      {/* <div
+      <div
         className="strawpoll-embed w-full h-[60vh] sm:h-[70vh] max-w-xs sm:max-w-lg flex flex-col"
         id="strawpoll_7MZ0kovwmgo"
         style={{
@@ -40,9 +40,8 @@ const GameTwo = () => {
         <script
           async
           src="https://cdn.strawpoll.com/dist/widgets.js"
-          charSet="utf-8"
         ></script>
-      </div> */}
+      </div>
     </div>
   );
 };
